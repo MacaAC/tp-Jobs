abed29540495859ae37469933fad2750225bf041
María Macarena Alvarez Castillo <mariamacarenaac@gmail.com>
2022-11-28 13:16:36 -0300

    preload form

--- jobs.js
+++ jobs.js
@@ -3,7 +3,7 @@ const $$ = (selector)=> document.querySelectorAll(selector)
 
 const hideElement = (selector) => selector.classList.add("hidden")
 const showElement = (selector) => selector.classList.remove("hidden")
-
+let isEdit = false
 
 const getJobsWithAsyncAwait = async () => {
     const response = await fetch("https://637fb96d8efcfcedacf6375c.mockapi.io/jobs")
@@ -21,20 +21,27 @@ getJobsWithAsyncAwait().then(data=>jobsCards(data))
 // }
 // getJobs()
 
-const getJob = (idJob) => {
-    fetch(`https://637fb96d8efcfcedacf6375c.mockapi.io/jobs/${idJob}`)
-        .then(res => res.json())
-        .then(data => {
-            //console.log(data)
-            viewDetails(data)
+// const getJob = (idJob) => {
+//     fetch(`https://637fb96d8efcfcedacf6375c.mockapi.io/jobs/${idJob}`)
+//         .then(res => res.json())
+//         .then(data => {
 
-            // if (jobId === "") {
-            //     renderUsers(data)
-            // } else {
-            //     populateForm(data)
-            // }
-        })
+//             if (isEdit) {
+//             showForm(data)
+//             } else {
+//             viewDetails(data)
+//             console.log(data)
+//             }
+//         })
+// }
+
+const getJobWithAsyncAwait = async (idJob) => {
+    const response = await fetch(`https://637fb96d8efcfcedacf6375c.mockapi.io/jobs/${idJob}`)
+    const job = await response.json()
+    return job
 }
+
+
 
 
 
@@ -67,8 +74,17 @@ const jobsCards = (arrayJobs) => {
                <div id="categoryDiv" class="m-1 bg-yellow-400 text-center text-xs font-bold">${category}</div>
                <div id="seniorityDiv" class="m-1 bg-orange-400 text-center text-xs font-bold">${seniority}</div>
            </div>
-           <button  data-id="${id}" class="btnSeeDetails w-2/3 h-10 my-2 rounded-md shadow-md bg-blue-400 text-white font-bold" onclick="getJob(${id})">See Details</button>
+           <button  data-id="${id}" class="btnSeeDetails w-2/3 h-10 my-2 rounded-md shadow-md bg-blue-400 text-white font-bold">See Details</button>
        </div>`
+}
+for (const btn of $$(".btnSeeDetails")){
+    btn.addEventListener("click",()=>{
+        const jobId = btn.getAttribute("data-id")
+        getJobWithAsyncAwait(jobId).then(data=> viewDetails(data))
+
+    }
+    )
+
 }
 }
 
@@ -107,9 +123,22 @@ $("#navHome").addEventListener('click', () => {
 })
 
 
+const showForm = (job) =>{
+    console.log(job)
+    
+    $("#container").innerHTML = ""
+    showElement($("#editJobForm"))
+
+    $("#descriptionEdit").value = job.description
+    $("#titleEdit").value = job.name
+    $("#locationEdit").value = job.location
+    $("#categoryEdit").value = job.category
+    $("#seniorityEdit").value = job.seniority
+    $("#srcEdit").value = job.img
+}
 
 
-const viewDetails = (objJob=>{
+const viewDetails = (objJob) =>{
     const{name,description,location,category,seniority, img,id}= objJob
     $("#container").innerHTML = `
     <div id = "card-{id}" class="w-5/6 h-[450px] my-3 border border-2 rounded-md shadow-2xl sm:w-1/3 sm:m-3 md:w-1/4 lg:w-1/5 xl:w-1/6">
@@ -125,17 +154,24 @@ const viewDetails = (objJob=>{
             <div id="seniorityDiv" class="m-1 bg-orange-400 text-center text-xs font-bold">${seniority}</div>
         </div>
         <div class="flex w-full justify-center">
-            <button data-id="${id}" class="btnEditJob w-1/3 h-10 m-2 rounded-md shadow-md bg-green-400 text-white font-bold" onclick="showForm()">Edit</button>
+            <button data-id="${id}" class="btnEditJob w-1/3 h-10 m-2 rounded-md shadow-md bg-green-400 text-white font-bold" >Edit</button>
             <button  data-id="${id}" class="btnDeleteJob w-1/3 h-10 m-2 rounded-md shadow-md bg-red-400 text-white font-bold" onclick="getJob(${id})" >Delete</button>  
         </div>
        
     </div>`
-})
+    for (const btn of $$(".btnEditJob")){
+        btn.addEventListener("click",()=>{
+            const jobId = btn.getAttribute("data-id")
+            getJobWithAsyncAwait(jobId).then(data=> showForm(data))
+    
+        }
+        )
+    
+    }
 
-const showForm = () =>{
-    $("#container").innerHTML = ""
-    showElement($("#editJobForm"))
 }
+
+
 //-------funcion navbar responsive
 
 $("#btnMenu").addEventListener('click', () => $("#menu").classList.toggle('hidden'))

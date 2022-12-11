c269a21f6e1f3001daf26415f458712124df0b11
María Macarena Alvarez Castillo <mariamacarenaac@gmail.com>
2022-12-11 18:52:52 -0300

    layout details

--- js/jobs.js
+++ js/jobs.js
@@ -50,7 +50,7 @@ const jobsCards = (arrayJobs) => {
 
             <div id = "card-{id}" class="w-5/6 h-full my-3 border border-2 rounded-md shadow-2xl sm:w-1/3 sm:m-3 md:w-1/4 lg:w-1/5 xl:w-1/6">
             <figure class ="w-full h-1/3 flex mt-2 items-center">
-                <img class="w-full h-[170px]" src=${img}" alt="job">
+                <img class="w-full h-[170px]" src="${img}" alt="job">
             </figure>
             <div id ="contents" class="h-2/3 p-2 flex flex-col justify-center items-center">
                 <h3 class="text-xl font-bold underline">${name}</h3>
@@ -101,18 +101,22 @@ const saveJobEdit =()=>{
         location: $("#locationEdit").value ,
         category:$("#categoryEdit").value ,
         seniority:$("#seniorityEdit").value,
-        img: $("#srcEdit").value ,
+        img: $("#srcEdit").value.toLowerCase() ,
     }
 }
 
 
 $("#navJob").addEventListener('click', () =>{
     hideElement($("#filters"))
+    hideElement($("#editJobForm"))
+
     $("#container").innerHTML= ` <div id="spinner" class="flex justify-center hidden">
     <p class="w-40 h-12 rounded-md shadow-md bg-indigo-500 text-white font-bold flex justify-center items-center"><span><i class="fas fa-spinner mx-2 text-3xl animate-spin"></i></span>Cargando...</p>
   </div>
   `
     // resetForm()
+   // $("#formNewJob").reset() no me permite hacer el reset de esta forma..que desgracia
+
     showElement($("#formNewJob"))
 })
 
